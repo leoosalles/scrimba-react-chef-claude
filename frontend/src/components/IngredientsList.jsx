ba5f3059c4090a4399b8537a13e67c00879e0eda
Leonardo Salles <leonardo.sdo87@gmail.com>
2025-10-06 21:02:58 -0300

    feat(ingredients-list): toggle recipe generation and reset via button
    Implements conditional logic to switch button behavior between fetching a recipe and resetting the app. Enhances usability by adapting the interface to the current recipe state.

--- frontend/src/components/IngredientsList.jsx
+++ frontend/src/components/IngredientsList.jsx
@@ -1,7 +1,11 @@
-function IngredientsList({ ingredients, fetchRecipe }) {
+function IngredientsList({ ingredients, fetchRecipe, resetApp, recipeShown }) {
   const ingredientsListItems = ingredients.map((ingredient, index) => (
     <li key={index}>{ingredient}</li>
   ));
+
+  const buttonLabel = recipeShown ? "New recipe" : "Get a recipe";
+
+  const handleClick = recipeShown ? resetApp : fetchRecipe
 
   return (
     <section className="recipe-container">
@@ -16,7 +20,7 @@ function IngredientsList({ ingredients, fetchRecipe }) {
             <h3>Ready for a recipe?</h3>
             <p>Generate a recipe from your list of ingredients.</p>
           </div>
-          <button onClick={fetchRecipe}>Get a recipe</button>
+          <button onClick={handleClick}>{buttonLabel}</button>
         </div>
       )}
     </section>
